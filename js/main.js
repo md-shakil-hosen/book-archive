f58055692052ea38f1dd6147030ae2302b37562e
md-shakil-hosen <shakilinfo91@gmail.com>
2021-09-02 16:50:12 +0600

    Error Handaling Pending

--- js/main.js
+++ js/main.js
@@ -1,48 +1,32 @@
-
-// Search Function 
-const searchInput = () => {
-    const searchInput = document.getElementById('search-Input');
-    const searchText = searchInput.value;
-    searchInput.value = '';
+let searchItem = () => {
+    let result = document.getElementById('result')
+    const searchBtn = document.getElementById('search-button')
+    let inputField = document.getElementById('input-field')
+    let inputData = inputField.value;
+    inputField.value = '';
+    let url = `http://openlibrary.org/search.json?q=${inputData}`;
+    fetch(url)
+        .then(res => res.json())
+        .then(data => displayBook(data.docs.slice(0, 50)))
 }
-// Book Data Load 
-const url = `http://openlibrary.org/search.json?q=javascript`;
-fetch(url)
-    .then(res => res.json())
-    .then(data => displayBookResult(data.docs))
-
-const displayBookResult = docs => {
-    const searchResult = document.getElementById('search-result');
-    searchResult.textContent = '';
-
-    // book Search 
-    docs.forEach(doc => {
-        console.log(doc.title)
-        const div = document.createElement('div');
+// Show Display 
+const displayBook = books => {
+    const bookField = document.getElementById('book-field');
+    books.forEach(book => {
+        // Show Display Book Result 0 - 30
+        result.innerHTML = `<h2 class = "text-center ">Search Result ${books.length} Found</h2>`;
+        const div = document.createElement('div')
         div.classList.add('col');
         div.innerHTML = `
-        <img src="${'https://covers.openlibrary.org/b/id/554106-M.jpg'}" class="card-img-top" alt="...">
-        `;
-        searchResult.appendChild(div)
-
+                <img src=" https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg " class="card-img-top" id="image" alt="Image">
+                <div class="card-body">
+                <h6 class="card-title"><span>Book Title: </span>${book.title}</h6>
+                  <h6 class="card-title">Author name: ${book.author_name}</h6>
+                  <h6 class="card-title">First publish: ${book.first_publish_year}</h6>
+                  <h6 class="card-title">Publisher: ${book.publisher}</h6>
+                </div>
+            `
+        bookField.appendChild(div);
 
     });
-}
-
-// book details 
-const displayBookDetails = doc => {
-    console.log(doc)
-    const bookDetails = document.getElementById('book-details');
-    bookDetails.textContent = '';
-    const div = document.createElement('div');
-    div.classList.add('card');
-    div.innerHTML = `
-    <div class="card-body">
-    <h4>${doc.subject}</h4>
-    <h5>${doc.author_name}</h5>
-        <h6 class="card-title">${doc.publish_date}</h6>
-    </div>
-</div>
-    `;
-    bookDetails.appendChild(div);
 }
